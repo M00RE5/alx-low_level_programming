549af687ebaa2136aea0b5a1b99ddd48151c332f
M00RE5 <dannymoore405@gmail.com>
2022-09-08 20:22:20 -0700

    coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/**
+* main -print the main function
+*
+*Description: using the main function
+*
+*this program prints  "Programming is like building a multilingual puzzle
+*return:0
+*/
+int main(void)
+{
+                                                                   return:0
+}
